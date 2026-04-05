e37f11cb48a50f44abaf487d4b7b72ea2a185369
cappycatt <aayushma61aryal@gmail.com>
2026-04-05 16:59:16 +0545

    Login ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Login from './Components/Login'
+import React from "react";
+import ThemeButton from "./Components/ThemeButton";
+import UserContext
 
 
 function App() {
   return (
     <div>
-      <h1>Notice</h1>
-      <p>Dashbord</p>
+      <UserContext.Provider value={{theme,toggle}}>
+      <Login/>
+      <ThemeButton/>
+      </UserContext.Provider>
     </div>
   );
 }
