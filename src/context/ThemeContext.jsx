cab1c1cc2af9ff8fbcae7885b57b8cc402fe26e1
cappycatt <aayushma61aryal@gmail.com>
2026-04-07 08:22:48 +0545

    theme button

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-   export const UserContext = createContext();
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggle = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <UserContext.Provider value={{ theme, toggle }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
