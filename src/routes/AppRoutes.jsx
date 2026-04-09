6fbc1ad446c74f2ad28f7f9ec8f57b5a2dbdd1d3
cappycatt <aayushma61aryal@gmail.com>
2026-04-09 11:39:46 +0545

    commit before merging

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import ProtectedRoute from "../components/ProtectedRoute";
+import CreateNotices from "../components/CreateNotices";
+import ManageNotices from "../components/ManageNotices";
 
 function AppRoutes() {
   return (
@@ -9,24 +11,9 @@ function AppRoutes() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/create-notice" element={<CreateNotices />} />
+      <Route path="/manage-notice" element={<ManageNotices />} />
 
-      <Route
-        path="/admin-dashboard"
-        element={
-          <ProtectedRoute allowedRole="admin">
-            <h1 className="p-10 text-3xl font-bold">Admin Dashboard</h1>
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/user-dashboard"
-        element={
-          <ProtectedRoute allowedRole="user">
-            <h1 className="p-10 text-3xl font-bold">User Dashboard</h1>
-          </ProtectedRoute>
-        }
-      />
     </Routes>
   );
 }
