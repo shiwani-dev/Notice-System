b9184769838505b1f1dff3d7211b0b7bb9b48851
shiwani <shiwanituladhar4@gmail.com>
2026-04-09 11:57:33 +0545

    dash

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,11 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import ProtectedRoute from "../components/ProtectedRoute";
-<<<<<<< HEAD
 import DashBoard from "../pages/DashBoard";
-=======
-import CreateNotices from "../components/CreateNotices";
->>>>>>> 9246152b4600aa52a3059a5bd99a914d8f1068c7
 
 function AuthRoutes() {
   return (
@@ -14,11 +10,7 @@ function AuthRoutes() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
       <Route path="/dashboard" element={<DashBoard/>}/>
-=======
-      <Route path="/create-notice" element={<CreateNotices />} />
->>>>>>> 9246152b4600aa52a3059a5bd99a914d8f1068c7
 
       <Route
         path="/dashboard"
